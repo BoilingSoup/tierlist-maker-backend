a15e53b1b9d292dfb7e6a475667a0a2b86364b7c
Karan Ahlawat <ahlawatkaran12@gmail.com>
2023-02-28 17:41:30 +0530

    [update] Username should be at most 20 chars

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid();
             $table->string('email')->unique();
-            $table->string('username')->unique();
+            $table->string('username', 20)->unique();
             $table->string('password');
             $table->boolean('is_admin');
             $table->timestamps();
