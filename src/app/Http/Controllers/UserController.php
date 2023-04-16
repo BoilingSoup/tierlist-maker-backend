ecf77608048a64e301164a3f047082f56fc61337
BoilingSoup <boilingsoup@protonmail.com>
2023-04-16 15:07:03 -0700

    Implement change password action

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChangePasswordRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -30,13 +32,32 @@ class UserController extends Controller
   public function update(UpdateUserRequest $request)
   {
     $validated = $request->validated();
+
     if (count($validated) !== 1) {
       abort(422);
+    }
+
+    $isOauth = (bool) Auth::user()->getOAuthProvider();
+
+    if (array_key_first($validated) === 'email' && $isOauth) {
+      abort(403);
     }
 
     $user = $this->repository->update($validated);
 
     return new UserResource($user);
+  }
+
+  public function changePassword(ChangePasswordRequest $request)
+  {
+    $validated = $request->validated(); // validation will throw an error if current_password is incorrect.
+
+    $isOauthAccount = (bool) Auth::user()->getOAuthProvider();
+    abort_if($isOauthAccount, 403);
+
+    $this->repository->changePassword($validated);
+
+    return response()->noContent();
   }
 
   /**
---
+++ src/app/Http/Requests/ChangePasswordRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
+class ChangePasswordRequest extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return Auth::check();
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+   */
+  public function rules(): array
+  {
+    return [
+        'current_password' => ['bail', 'required', 'current_password'],
+        'password' => ['required', 'confirmed', Password::defaults()],
+    ];
+  }
+}
