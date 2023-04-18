a808bef120310a16ad36b70895642c5d20de4731
BoilingSoup <boilingsoup@protonmail.com>
2023-04-18 12:14:30 -0700

    Remove username unique constraint and validations

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,50 +8,50 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create(User::TABLE, function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            // reddit oauth doesn't return email. Enforce NOT NULL email for non-OAuth users by validating the register form submissions
-            $table->string('email', MaxLength::USERS_EMAIL)->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('username')->unique();
-            $table->string('password')->nullable();
-            $table->boolean('is_admin')->default(false);
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create(User::TABLE, function (Blueprint $table) {
+      $table->uuid('id')->primary();
+      // reddit oauth doesn't return email. Enforce NOT NULL email for non-OAuth users by validating the register form submissions
+      $table->string('email', MaxLength::USERS_EMAIL)->nullable();
+      $table->timestamp('email_verified_at')->nullable();
+      $table->string('username');
+      $table->string('password')->nullable();
+      $table->boolean('is_admin')->default(false);
 
-            $table->string('github_id')->nullable();
-            $table->string('github_token')->nullable();
-            $table->string('github_refresh_token')->nullable();
+      $table->string('github_id')->nullable();
+      $table->string('github_token')->nullable();
+      $table->string('github_refresh_token')->nullable();
 
-            $table->string('gitlab_id')->nullable();
-            $table->string('gitlab_token')->nullable();
-            $table->string('gitlab_refresh_token')->nullable();
+      $table->string('gitlab_id')->nullable();
+      $table->string('gitlab_token')->nullable();
+      $table->string('gitlab_refresh_token')->nullable();
 
-            $table->string('google_id')->nullable();
-            $table->string('google_token')->nullable();
-            $table->string('google_refresh_token')->nullable();
+      $table->string('google_id')->nullable();
+      $table->string('google_token')->nullable();
+      $table->string('google_refresh_token')->nullable();
 
-            $table->string('reddit_id')->nullable();
-            $table->string('reddit_token')->nullable();
-            $table->string('reddit_refresh_token')->nullable();
+      $table->string('reddit_id')->nullable();
+      $table->string('reddit_token')->nullable();
+      $table->string('reddit_refresh_token')->nullable();
 
-            $table->string('discord_id')->nullable();
-            $table->string('discord_token')->nullable();
-            $table->string('discord_refresh_token')->nullable();
+      $table->string('discord_id')->nullable();
+      $table->string('discord_token')->nullable();
+      $table->string('discord_refresh_token')->nullable();
 
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+      $table->rememberToken();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('users');
+  }
 };
