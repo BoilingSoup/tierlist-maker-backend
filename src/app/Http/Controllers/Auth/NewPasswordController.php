08986f9b7a1ecd0f66d806654d0081d673821ffa
BoilingSoup <boilingsoup@protonmail.com>
2023-04-19 16:38:22 -0700

    Tweak reset password controller to work with non-unique email

--- src/app/Http/Controllers/Auth/NewPasswordController.php
+++ src/app/Http/Controllers/Auth/NewPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -10,44 +11,42 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules;
-use Illuminate\Validation\ValidationException;
 
 class NewPasswordController extends Controller
 {
-    /**
-     * Handle an incoming new password request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): JsonResponse
-    {
-        $request->validate([
-            'token' => ['required'],
-            'email' => ['required', 'email'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+  /**
+   * Handle an incoming new password request.
+   *
+   * @throws \Illuminate\Validation\ValidationException
+   */
+  public function store(Request $request): JsonResponse
+  {
+    $request->validate([
+        'token' => ['required'],
+        'email' => ['required', 'email'],
+        'password' => ['required', 'confirmed', Rules\Password::defaults()],
+    ]);
 
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            function ($user) use ($request) {
-                $user->forceFill([
-                    'password' => Hash::make($request->password),
-                    'remember_token' => Str::random(60),
-                ])->save();
-
-                event(new PasswordReset($user));
-            }
-        );
-
-        if ($status != Password::PASSWORD_RESET) {
-            throw ValidationException::withMessages([
-                'email' => [__($status)],
-            ]);
-        }
-
-        return response()->json(['status' => __($status)]);
+    /** @var User */
+    $user = User::where('email', $request->email)->wherePasswordIsNotNull()->first();
+    if (is_null($user)) {
+      abort(400, 'Invalid request');
     }
+
+    $tokenIsValid = Password::getRepository()->exists($user, $request->token);
+    if (! $tokenIsValid) {
+      abort(400, 'Invalid request');
+    }
+
+    $user->forceFill([
+        'password' => Hash::make($request->password),
+        'remember_token' => Str::random(60),
+    ])->save();
+
+    event(new PasswordReset($user));
+
+    Password::getRepository()->delete($user);
+
+    return response()->json(['status' => 'Your password was reset successfully.']);
+  }
 }
