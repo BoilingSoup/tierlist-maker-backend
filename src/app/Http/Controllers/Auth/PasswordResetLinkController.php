e3b766dbf419807f4e06b06e2e98e1ff288bd0a7
BoilingSoup <boilingsoup@protonmail.com>
2023-04-19 13:50:36 -0700

    Update password reset controller

--- src/app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ src/app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -3,37 +3,37 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
-use Illuminate\Validation\ValidationException;
 
 class PasswordResetLinkController extends Controller
 {
-    /**
-     * Handle an incoming password reset link request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): JsonResponse
-    {
-        $request->validate([
-            'email' => ['required', 'email'],
-        ]);
+  /**
+   * Handle an incoming password reset link request.
+   *
+   * @throws \Illuminate\Validation\ValidationException
+   */
+  public function store(Request $request): JsonResponse
+  {
+    $request->validate([
+        'email' => ['required', 'email'],
+    ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+    // We will send the password reset link to this user. Once we have attempted
+    // to send the link, we will examine the response then see the message we
+    // need to show to the user. Finally, we'll send out a proper response.
 
-        if ($status != Password::RESET_LINK_SENT) {
-            throw ValidationException::withMessages([
-                'email' => [__($status)],
-            ]);
-        }
-
-        return response()->json(['status' => __($status)]);
+    /** @var User */
+    $user = User::where('email', $request->email)->wherePasswordIsNotNull()->first();
+    if (is_null($user)) {
+      abort(400, 'Invalid request');
     }
+
+    $token = Password::getRepository()->create($user);
+    $user->sendPasswordResetNotification($token);
+
+    return response()->json(['status' => 'We have emailed your password reset link.']);
+  }
 }
